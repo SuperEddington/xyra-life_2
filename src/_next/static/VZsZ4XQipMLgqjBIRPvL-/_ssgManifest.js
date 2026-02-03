d982be85d9253b43a504769c69547c558e5fd1f5
SuperEddington <35928135+SuperEddington@users.noreply.github.com>
2026-02-03 09:43:33 +0800

    Delete src/_next/static directory

--- src/_next/static/VZsZ4XQipMLgqjBIRPvL-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fproduct\u002F[id]","\u002F[locale]\u002Fshop"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
